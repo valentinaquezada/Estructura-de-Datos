9ab4f81342ae97543c5635233608f19b28292eeb
valentinaquezada <159058663+valentinaquezada@users.noreply.github.com>
2024-03-04 13:35:45 -0400

    Create Estudiante.h

---
+++ Estudiante.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Estudiante {
+private:
+    string nombre;
+    int edad;
+    string materia;
+    float calificacion;
+public:
+    Estudiante();
+    ~Estudiante();
+    void set_nombre(string _n);
+    string get_nombre();
+};
